4a50beb6a5ee4f997f372755796d2248dd967e1b
John Binning <john.binning17@gmail.com>
2017-05-08 14:48:41 -0600

    add fetch getting a respons

--- swapi-box/src/App/App.js
+++ swapi-box/src/App/App.js
@@ -7,6 +7,22 @@ import { CardHolder } from './Components/CardHolder/CardHolder.js'
 import { Favorites } from './Components/Favorites/Favorites.js'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      favorites: [],
+      scroll: [],
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://swapi.co/api/films/')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data )
+      })
+  }
+
   render() {
     return (
       <div className="App">
