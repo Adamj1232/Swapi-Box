aece0b43636a08e947036043de4756790b60542b
Adam Carpenter <adamcarpenter86@gmail.com>
2017-05-08 22:53:50 -0600

    passing in worldData and the rest of the data shouldnt be hard to get for the person card

--- swapi-box/src/App/helpers/Cleaner.js
+++ swapi-box/src/App/helpers/Cleaner.js
@@ -11,32 +11,30 @@ export default class Cleaner {
 
   peopleCleaner(data){
     let peopleResults = data.results.map( person => {
-      // console.log(person)
-      // let worldName = fetch(person.homeworld)
-      // .then((response) => response.json())
-      // .then((data) => {
-      //   if(data){
-      //   console.log(data.name)
-      //   return data.name
-      //   }
-      // })
+      console.log(person.homeworld)
+
+        // this.getHomeworldData(person.homeworld)
       return {
         person: person.name,
-        // homeworld: worldName
+        speciesAPI: person.species[0],
+        homeAPI: person.homeworld,
+        homeworld: this.getHomeworldData(person.homeworld)
       }
     })
     return peopleResults
   }
 
-  // homeworldCleaner(person){
-  //   let worldName = fetch(person.homeworld)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     if(data){
-  //     console.log(data.name)
-  //     return data.name
-  //     }
-  //   })
-  // }
+  getHomeworldData(api){ //put on page holding cards??
+    var homeWorlds = []
+     fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data.name)
+       homeWorlds.push({data: data.name, pop: data.population})
+       })
+    console.log(Object.values(homeWorlds))
+    return homeWorlds
+  // })
+}
 
 }
