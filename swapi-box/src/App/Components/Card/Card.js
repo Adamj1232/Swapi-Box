839c02b8ade8f49aa18b0ed780ad6e444dc829d9
Adam Carpenter <adamcarpenter86@gmail.com>
2017-05-10 13:45:34 -0600

    passing props down to card working, catch needs help and cards need styling

--- swapi-box/src/App/Components/Card/Card.js
+++ swapi-box/src/App/Components/Card/Card.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 
-export const Card = ({name, homeworld, species, population}) => {
-
-  return(
-    <div>
-      <div>Name: {name}</div>
-      <div>Species: {species}</div>
-      <div>Homeworld: {homeworld}</div>
-      <div>Population: {population}</div>
-    </div>
-  )
+export const Card = ({name, homeworld, species, population, selected, terrain, climate, residents, model, vehicleClass, passengers}) => {
+  if ( selected === 'people'){
+    return(
+      <section className='card'>
+        <div>Name: {name}</div>
+        <div>Species: {species}</div>
+        <div>Homeworld: {homeworld}</div>
+        <div>Population: {population}</div>
+      </section>
+    )
+  } else if ( selected === 'planets'){
+    return(
+      <section className='card'>
+        <div>Name: {name}</div>
+        <div>Terrain: {terrain}</div>
+        <div>Population: {population}</div>
+        <div>Climate: {climate}</div>
+        <div>Residents: {residents}</div>
+      </section>
+    )
+  } else if ( selected === 'vehicles'){
+    return(
+      <section className='card'>
+        <div>Name: {name}</div>
+        <div>Model: {model}</div>
+        <div>Class: {vehicleClass}</div>
+        <div>Passengers: {passengers}</div>
+      </section>
+    )
+  } else {
+    return (
+      <div></div>
+    )
+  }
 }
 
 // function wait(){
--- swapi-box/src/App/Components/CardHolder/CardHolder.js
+++ swapi-box/src/App/Components/CardHolder/CardHolder.js
@@ -2,12 +2,12 @@ import React from 'react'
 import {Card} from '../Card/Card.js'
 
 export const CardHolder = ({selected, peopleData, planetData, vehicleData, peopleAtrributes}) => {
-  if(selected === '' ){
-    return <div>Loading....</div>
-  }
-  // if(peopleData.length === 0 || !planetData.length || !vehicleData.length ){
+  // if(selected === '' ){
   //   return <div>Loading....</div>
-  // } only run when making all api calls
+  // }
+  if(peopleData.length === 0 || !planetData.length || !vehicleData.length ){
+    return <div>Loading....</div>
+  } //only run when making all api calls
 
   if(selected === 'people'){
     console.log('people')
@@ -19,6 +19,7 @@ export const CardHolder = ({selected, peopleData, planetData, vehicleData, peopl
         species={person.species}
         population={person.population2}
         key={person.name2}
+        selected={selected}
       />
     )
   })
@@ -29,16 +30,51 @@ export const CardHolder = ({selected, peopleData, planetData, vehicleData, peopl
       <div>{people}</div>
     </section>
   )
-} else if (selected === 'planets'){
-  console.log('planets')
-  return(
-    <div>Planets</div>
-  )
-} else if (selected === 'vehicles'){
-  console.log('vehicles')
-  return(
-    <div>Vehicles</div>
-  )
-}
+  } else if (selected === 'planets'){
+    console.log('planets')
+    let planets = planetData.map((planet) => {
+      return(
+        <Card
+          name={planet.name}
+          terrain={planet.terrain}
+          population={planet.population}
+          climate={planet.climate}
+          residents={planet.residents}
+          key={planet.name}
+          selected={selected}
+        />
+      )
+    })
 
+    return (
+      <section>
+        <h2>Planets</h2>
+        <div>{planets}</div>
+      </section>
+    )
+  } else if (selected === 'vehicles'){
+    console.log('vehicles')
+    let vehicles = planetData.map((vehicle) => {
+      return(
+        <Card
+          name={vehicle.name}
+          model={vehicle.model}
+          vehicleClass={vehicle.class}
+          passengers={vehicle.passengers}
+          key={vehicle.name}
+          selected={selected}
+        />
+      )
+    })
+
+    return (
+      <section>
+        <h2>Vehicles</h2>
+        <div>{vehicles}</div>
+      </section>
+    )
+  }
+  return (
+    <section></section>
+  )
 }
