1ab323d9bf4597d02865e7089dc9fc95ddfb83ef
John Binning <john.binning17@gmail.com>
2017-05-11 10:49:03 -0600

    add test mock data

--- swapi-box/src/App/App.test.js
+++ swapi-box/src/App/App.test.js
@@ -1,8 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import fetchMock from 'fetch-mock'
+import { shallow, mount } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import { mockedFilms, mockedPeople, mockedVehicles, mockedPlanets } from '../TestData'
+
+describe('App instantiation', () => {
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+
+  })
+
+  it('should have initial states start empty values',  () => {
+
+    const wrapper = mount(<App />)
+
+    // await wrapper.update()
+
+    expect(wrapper.state('filmData')).toEqual({})
+    expect(wrapper.state('selected')).toEqual('')
+    expect(wrapper.state('favorites')).toEqual([])
+    expect(wrapper.state('vehicles')).toEqual([])
+    expect(wrapper.state('people')).toEqual([])
+    expect(wrapper.state('peopleAtrributes')).toEqual([])
+    expect(wrapper.state('planets')).toEqual([])
+  })
+
+})
+
+describe('App functionality', () => {
+
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([])
+    fetchMock.restore()
+  })
+
+  const mockCalls = (() => {
+    console.log(mockedFilms)
+    fetchMock.get('http://www.swapi.co/api/films', {
+      status: 200,
+      body: mockedFilms,
+      throws: 'rejected'
+    }).catch('error')
+    fetchMock.get('http://www.swapi.co/api/people', {
+      status: 200,
+      body: mockedPeople,
+      throws: 'rejected'
+    }).catch('error')
+    fetchMock.get('http://www.swapi.co/api/vehicles', {
+      status: 200,
+      body: mockedVehicles,
+      throws: 'rejected'
+    }).catch('error')
+    fetchMock.get('http://www.swapi.co/api/planets', {
+      status: 200,
+      body: mockedPlanets,
+      throws: 'rejected'
+    }).catch('error')
+  })
+
+  function wait(){
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve()
+      }, 2000)
+    })
+  }
+  it('should have filmData after mounting', () => {
+    mockCalls()
+    const wrapper = mount(<App />)
+
+    const found = wrapper.find('App')
+    const keys = Object.keys(found.state.filmData)
+    expect(true)
+    // expect(keys.length).toEqual(3)
+  })
+
+})
